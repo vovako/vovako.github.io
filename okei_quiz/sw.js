4b769ae5db4857404f2e504dea3c1ca1f03fb909
vovako <vladimirkondaurov38@gmail.com>
2024-03-07 17:13:34 +0500

    fix

--- okei_quiz/sw.js
+++ okei_quiz/sw.js
@@ -1,7 +1,33 @@
-self.addEventListener('install', () => {
-	console.log('[SW] install');
+const STATIC_CACHE_NAME = 'offline-cache'
+let assetsUrls = [
+	'./index.html',
+]
+
+self.addEventListener('install', async (event) => {
+	console.log('[SW] install', event);
+	
+	const list = window.performance.getEntriesByType('resource')
+	list.forEach(i => {
+		const result = i.name.match(/\/([^\/^\?]+)(\?[^\?]*)?$/)[1]
+		console.log(result);
+	})
+
+	const cache = await caches.open(STATIC_CACHE_NAME)
+	await cache.addAll(assetsUrls)
 })
 
-self.addEventListener('activate', () => {
-	console.log('[SW] activate');
-})
+self.addEventListener('activate', function (event) {
+	console.log('[SW] Activating Service Worker ....', event);
+	event.waitUntil(
+		caches.keys()
+			.then((keyList) => {
+				return Promise.all(keyList.map((key) => {
+					if (key !== STATIC_CACHE_NAME) {
+						console.log('[Service Worker] Removing old cache.', key);
+						return caches.delete(key);
+					}
+				}));
+			})
+	);
+	return self.clients.claim();
+});
